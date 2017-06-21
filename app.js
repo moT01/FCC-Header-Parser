382e84270f09b196d33238e3c37ebcb998c9368f
moT01 <TMondloch01@gmail.com>
2017-06-21 18:32:40 -0500

    fixed ip address finder - hopefully

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 var express = require('express');
 var useragent = require('useragent');
-var ip = require('ip');
+//var ip = require('ip');
 
 var app = express();
 
 app.get('/', function(req, res) {
+    var ip = (req.headers['x-forwarded-for'] ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress).split(",")[0];
+
   res.json({
       "Software": (useragent.parse(req.headers['user-agent'])).toString(),
       "Language": req.acceptsLanguages(),
-      "IP Address": ip.address()
+      "IP Address": ip
   }); //end res.json
 }); //end app.get
 
